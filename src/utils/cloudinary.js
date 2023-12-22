94d0576fd7926e6b7dd01660a62c9c87746e56b6
AbdulAhadera <abdulahad.era@gmail.com>
2023-12-22 00:42:47 -0600

    project setup in progress

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: 'ddmxpjosk',  // yeh sab env variable mein rkhayege
+    api_key: '494183635338429', // yeh sab env variable mein rkhayege
+    api_secret: 'PpTj-5tPLZXUKpyxUXb_GK3ihdc' // yeh sab env variable mein rkhayege
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        // file has been uploaded succesfully
+        console.log("file is uploaded on cloudinary", response.url);
+        return response
+
+    } catch (error) {
+        fs.unlink(localFilePath) 
+        // removed the locally saved temporary file as the ipload operation got failed
+        return null
+
+    }
+
+}
+
+
+export {uploadOnCloudinary};
